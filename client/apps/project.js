4b828f70182d982eefce6fe2e891d2e1f2f2407b
Chris Ryan <chrisryanassociates@gmail.com>
2023-03-14 21:26:24 -0600

    redesign

--- client/apps/project.js
+++
@@ -1,6 +0,0 @@
-const projectMenuIcon = document.querySelector('.menu-icon');
-const projectContainer = document.querySelector('.project-container');
-
-projectMenuIcon.addEventListener('click', () => {
-    projectContainer.classList.toggle('change')
-});
--- client/main.js
+++ client/main.js
@@ -1,6 +1,20 @@
-const menuIcon = document.querySelector('.menu-icon');
-const container = document.querySelector('.container');
+const nav = document.getElementById('nav');
+const specificHeader = document.querySelector('.skills');
 
-menuIcon.addEventListener('click', () => {
-    container.classList.toggle('change')
+window.addEventListener('scroll', () => {
+  const scrollY = window.scrollY;
+  const navHeight = nav.clientHeight;
+  const specificHeaderTop = specificHeader.offsetTop;
+
+  if (scrollY >= specificHeaderTop - navHeight) {
+    nav.classList.add('nav-bg');
+  } else {
+    nav.classList.remove('nav-bg');
+  }
+});
+
+const scrollToTopButton = document.getElementById('scroll-to-top');
+
+scrollToTopButton.addEventListener('click', () => {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
 });
