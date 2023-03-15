0b936b58baef2d6d110667d61985840859e06549
Chris Ryan <chrisryanassociates@gmail.com>
2023-03-15 15:44:20 -0600

    responsive

--- main.js
+++ main.js
@@ -1,17 +1,35 @@
 const nav = document.getElementById('nav');
 const specificHeader = document.querySelector('.skills');
 
-window.addEventListener('scroll', () => {
+function updateNavBackground() {
   const scrollY = window.scrollY;
   const navHeight = nav.clientHeight;
   const specificHeaderTop = specificHeader.offsetTop;
+  const screenWidth = window.innerWidth;
 
-  if (scrollY >= specificHeaderTop - navHeight) {
-    nav.classList.add('nav-bg');
+  if (screenWidth <= 700) {
+    if (scrollY > 0) {
+      nav.classList.add('nav-bg');
+    } else {
+      nav.classList.remove('nav-bg');
+    }
   } else {
-    nav.classList.remove('nav-bg');
+    if (scrollY >= specificHeaderTop - navHeight) {
+      nav.classList.add('nav-bg');
+    } else {
+      nav.classList.remove('nav-bg');
+    }
   }
-});
+}
+
+// Add scroll event listener
+window.addEventListener('scroll', updateNavBackground);
+
+// Define the media query
+const mediaQuery = window.matchMedia('(max-width: 700px)');
+
+// Add media query change event listener
+mediaQuery.addEventListener('change', updateNavBackground);
 
 const scrollToTopButton = document.getElementById('scroll-to-top');
 
